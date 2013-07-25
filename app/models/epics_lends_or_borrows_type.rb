5ceae4b9c136bd573b354bda44728f796de88b78
Blessings <blessings.mhango@gmail.com>
2013-07-25 12:20:16 +0200

    removed comma causing error

--- app/models/epics_lends_or_borrows_type.rb
+++ app/models/epics_lends_or_borrows_type.rb
@@ -1,4 +1,4 @@
 class EpicsLendsOrBorrowsType < ActiveRecord::Base
-	set_table_name :epics_lends_or_borrows_types, 
+	set_table_name :epics_lends_or_borrows_types 
 	set_primary_key :epics_lends_or_borrows_type_id
 end

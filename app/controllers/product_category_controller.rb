635bb5e64234591d8ee5fd92c866ad8e3f1b2361
Blessings <blessings.mhango@gmail.com>
2013-07-29 17:29:49 +0200

    built index new create edit update and void item category actions

--- app/controllers/product_category_controller.rb
+++ app/controllers/product_category_controller.rb
@@ -1,20 +1,49 @@
 class ProductCategoryController < ApplicationController
+
   def index
+    @product_category = EpicsProductCategory.all
+    render :layout => false
   end
 
   def new
+    @product_category = EpicsProductCategory.new()
   end
 
   def create
+    @product_category = EpicsProductType.new
+    @product_category.class = params[:product_category][:class]
+    @product_category.name = params[:product_category][:name]
+    @product_category.description = params[:product_category][:description]
+    if @product_category.save!
+       redirect_to :action => :index
+    else
+       redirect_to :action => :new
+    end
+
   end
 
   def edit
+    @product_category = EpicsProductCategory.find(params[:product_category])
   end
 
   def update
+    @product_category = EpicsProductCategory.find(params[:product_category][:product_category_id])
+    @product_category.class = params[:product_category][:class]
+    @product_category.name = params[:product_category][:name]
+    @product_category.description = params[:product_category][:description]
+
+    if @product_category.save
+       redirect_to :action => :index
+    else
+       redirect_to :action => :edit, :product_category_id => params[:product_category][:product_category_id]
+    end
   end
 
   def void
+    @product_category = EpicsProductCategory.find(params[:product_category_id])
+    @product_category.voided = 1
+    @product_category.save!
+		render :text => 'showMsg("Record Deleted!")'
   end
-
+  
 end

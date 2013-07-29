5f900b131054557dbb592ecb1e875b242a3bf747
Blessings <blessings.mhango@gmail.com>
2013-07-29 16:58:13 +0200

    product and product category relationship

--- app/models/epics_product_category.rb
+++ app/models/epics_product_category.rb
@@ -1,7 +1,6 @@
 class EpicsProductCategory < ActiveRecord::Base
   set_table_name :epics_product_category
-	set_primary_key :epics_product_order_id
+	set_primary_key :epics_product_category_id
   default_scope where('voided = 0')
-  belongs_to :epics_orders, :foreign_key => :epics_order_id, :conditions => {:voided => 0}
-  belongs_to :epics_stock_details, :foreign_key => :epics_stock_details_id, :conditions => {:voided => 0}
+  has_many :epics_products, :foreign_key => :epics_product_id, :conditions => {:voided => 0}
 end

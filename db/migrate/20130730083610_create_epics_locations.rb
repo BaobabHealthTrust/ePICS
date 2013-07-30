d6db21b032f27deed63b167079eb02605c27c4a5
Blessings <blessings.mhango@gmail.com>
2013-07-30 10:52:31 +0200

    built location models and migrations

--- db/migrate/20130730083610_create_epics_locations.rb
+++ db/migrate/20130730083610_create_epics_locations.rb
@@ -1,10 +1,10 @@
 class CreateEpicsLocations < ActiveRecord::Migration
   def self.up
-    create_table :epics_locations do |t|
+    create_table :epics_locations, :primary_key => :epics_location_id do |t|
       t.string :name
       t.string :description
       t.integer :epics_location_type_id
-
+      t.boolean :voided, :default => false
       t.timestamps
     end
   end
--- db/migrate/20130730083647_create_epics_location_types.rb
+++ db/migrate/20130730083647_create_epics_location_types.rb
@@ -1,9 +1,9 @@
 class CreateEpicsLocationTypes < ActiveRecord::Migration
   def self.up
-    create_table :epics_location_types do |t|
+    create_table :epics_location_types ,:primary_key => :epics_location_type_id do |t|
       t.string :name
       t.string :description
-
+      t.boolean :voided, :default => false
       t.timestamps
     end
   end

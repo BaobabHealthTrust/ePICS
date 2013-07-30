ee478182a6a29e8d653f3638cdcfdb13133f7c0a
Blessings <blessings.mhango@gmail.com>
2013-07-30 11:32:26 +0200

    built location and location type controllers

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -1,20 +1,50 @@
 class LocationController < ApplicationController
   def index
+    @locations = EpicsLocation.all
+    render :layout => false
   end
 
   def new
+    @locations = EpicsLocation.new
+    @location_type_map = EpicsLocationType.all.map{|location_type|[location_type.name,location_type.epics_location_type_id]}
   end
 
-  def create
+  def create 
+    @location = EpicsLocation.new
+    @location.name = params[:location][:name]
+    @location.description = params[:location][:description]
+    @location.epics_location_type_id = params[:location][:location_type_id]
+    if @location.save!
+       redirect_to :action => :index
+    else
+       redirect_to :action => :new
+    end
+
   end
 
   def edit
+    @location = EpicsLocation.find(params[:location])
+    @location_type_map = EpicsLocationType.all.map{|location_type|[location_type.name,location_type.epics_location_type_id]}
   end
 
   def update
+    @location = EpicsLocation.find(params[:location][:location_id])
+    @location.name = params[:location][:name]
+    @location.description = params[:location][:description]
+    @location.epics_location_type_id = params[:location][:location_type_id]
+    
+    if @location.save!
+       redirect_to :action => :index
+    else
+       redirect_to :action => :edit, :location_id => params[:location][:location_id]
+    end
   end
 
   def void
+    @location = EpicsLocation.find(params[:location_id])
+    @location.voided = 1
+    @location.save!
+		render :text => 'showMsg("Record Deleted!")'
   end
 
 end
--- app/controllers/location_type_controller.rb
+++ app/controllers/location_type_controller.rb
@@ -1,20 +1,45 @@
 class LocationTypeController < ApplicationController
   def index
+    @location_types = EpicsLocationType.all
+    render :layout => false
   end
 
   def new
+    @location_types = EpicsLocationType.new
   end
 
   def create
+    @location_type = EpicsLocationType.new
+    @location_type.name = params[:location_type][:name]
+    @location_type.description = params[:location_type][:description]
+    if @location_type.save!
+       redirect_to :action => :index
+    else
+       redirect_to :action => :new
+    end
+
   end
 
   def edit
+    @location_type = EpicsLocationType.find(params[:location_type])
   end
 
   def update
+    @location_type = EpicsLocationType.find(params[:location_type][:location_type_id])
+    @location_type.name = params[:location_type][:name]
+    @location_type.description = params[:location_type][:description]
+
+    if @location_type.save!
+       redirect_to :action => :index
+    else
+       redirect_to :action => :edit, :location_type_id => params[:location_type][:location_type_id]
+    end
   end
 
   def void
+    @location_type = EpicsLocationType.find(params[:location_type_id])
+    @location_type.voided = 1
+    @location_type.save!
+		render :text => 'showMsg("Record Deleted!")'
   end
-
 end

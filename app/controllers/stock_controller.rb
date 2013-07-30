9924b5215b9af16df406eb5821cf3dac244dc5c6
Blessings <blessings.mhango@gmail.com>
2013-07-30 16:01:10 +0200

    add create method

--- app/controllers/stock_controller.rb
+++ app/controllers/stock_controller.rb
@@ -7,7 +7,18 @@ class StockController < ApplicationController
   end
 
   def create
-    
+    session[:stock] = nil
+    stock_hash = Hash.new
+    stock_hash[:supplier_id] = params[:stock][:supplier_id]
+    stock_hash[:grn_number] = params[:stock][:grn_number]
+    stock_hash[:grn_date] = params[:stock][:grn_date]
+    session[:stock] = stock_hash
+
+    unless session[:stock].nil?
+      redirect_to :controller => :stock_details, :action => :new
+    else
+      redirect_to :action => :new
+    end
   end
 
   def edit

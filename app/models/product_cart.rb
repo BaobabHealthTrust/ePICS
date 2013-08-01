5b50f7a599b7914ad24007a649ead5dc9507b777
Blessings <blessings.mhango@gmail.com>
2013-08-01 10:00:31 +0200

    added item cart

---
+++ app/models/product_cart.rb
@@ -0,0 +1,25 @@
+
+class ProductCart
+  attr_reader :items
+
+  def initialize
+      @items = []
+  end
+
+def add_product(product, quantity)
+  current_item = @items.find {|item| item.product == product}
+  if current_item
+    current_item.increment_quantity
+  else
+    @items << ProductCartItem.new(product, quantity)
+  end
+end
+
+def remove_product(product)
+  current_item = @items.find {|item| item.product == product}
+  if current_item
+    @items.delete(current_item)
+  end
+end
+
+end

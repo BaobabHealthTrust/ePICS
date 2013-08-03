e9cbc7956686de8e554804cf436b9f39ed96c85e
Blessings <blessings.mhango@gmail.com>
2013-08-03 16:12:07 +0200

    witness names and stock relationship

--- app/models/epics_stock.rb
+++ app/models/epics_stock.rb
@@ -3,5 +3,6 @@ class EpicsStock < ActiveRecord::Base
   set_primary_key :epics_stock_id
   default_scope where('voided = 0')
   has_many :epics_stock_details
+  has_many :epics_witness_names
   belongs_to :epics_supplier, :foreign_key => :epics_supplier_id
 end

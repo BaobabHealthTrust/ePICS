230866febe58965b8d33716ad6297c01baf62345
Blessings <blessings.mhango@gmail.com>
2013-08-03 15:27:22 +0200

    working on witness names

--- db/migrate/20130803132156_create_epics_witness_names.rb
+++ db/migrate/20130803132156_create_epics_witness_names.rb
@@ -1,6 +1,8 @@
 class CreateEpicsWitnessNames < ActiveRecord::Migration
   def self.up
-    create_table :epics_witness_names do |t|
+    create_table :epics_witness_names, :primary_key => :epics_witness_name_id do |t|
+      t.integer :epics_stock_id
+      t.string :name
 
       t.timestamps
     end

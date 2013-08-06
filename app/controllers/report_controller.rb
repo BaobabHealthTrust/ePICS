dda08bd92a2ed599f977b4e6579bbd21a93e7c04
Ernest Matola <mangochiman@gmail.com>
2013-08-06 19:06:17 +0200

    Created a controller for handling reports

---
+++ app/controllers/report_controller.rb
@@ -0,0 +1,24 @@
+class ReportController < ApplicationController
+
+  def drug_availability
+    
+  end
+  def audit_report
+    
+  end
+  def daily_dispenstation
+    
+  end
+  def store_room
+
+  end
+  def received_items
+
+  end
+  def monthly_report
+    
+  end
+  def view_alerts
+    
+  end
+end

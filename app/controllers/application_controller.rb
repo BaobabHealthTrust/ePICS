5d285c88c1abbd754b02e279c9c05e9672de5b2f
timmtonga <timmtonga@gmail.com>
2013-08-07 19:31:55 +0200

    fixed links in login pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :perform_basic_auth, :except => ['login','logout','authenticate']                         
+  before_filter :perform_basic_auth, :except => ['login','logout','authenticate']
 
-  protected                                                                     
-                                                                                
-  def perform_basic_auth                                                        
-    if session[:user_id].blank?                                                 
-      respond_to do |format|                                                    
-        format.html { redirect_to :controller => 'user',:action => 'logout' }   
-      end                                                                       
-    elsif not session[:user_id].blank?                                          
+  protected
+
+  def perform_basic_auth
+    if session[:user_id].blank?
+      respond_to do |format|
+        format.html { redirect_to :controller => 'user',:action => 'logout' }
+      end
+    elsif not session[:user_id].blank?
       User.current_user = User.find(session[:user_id])
-    end                                                                         
-  end 
+    end
+  end
 
 end

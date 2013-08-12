3a1664bd2a0e966baf9f494ced0f4702e396a4e5
Mwatha Bwanali <mwathabwanali@gmail.com>
2013-08-12 11:45:09 +0200

    altr table

--- db/migrate/20130724165421_create_epics_lends_or_borrows.rb
+++ db/migrate/20130724165421_create_epics_lends_or_borrows.rb
@@ -2,9 +2,11 @@ class CreateEpicsLendsOrBorrows < ActiveRecord::Migration
   def self.up
     create_table :epics_lends_or_borrows, :primary_key => :epics_lends_or_borrows_id do |t|
 			t.integer :epics_orders_id
+			t.integer :epics_stock_id
 			t.integer :epics_lends_or_borrows_type_id
-			t.date :date_issued
-			t.integer :epics_stock_details_id
+			t.date :lend_or_borrow_date
+			t.date :return_date
+			t.boolean :reimbursed, :default => false
 			t.boolean :voided, :default => false
 			t.timestamps
 		end

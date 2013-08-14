fd196fb9cd3a33ab612db2d200a4d4e6e34d5803
Mwatha Bwanali <mwathabwanali@gmail.com>
2013-08-14 21:17:37 +0200

    adding reports

--- app/models/epics_report.rb
+++ app/models/epics_report.rb
@@ -8,7 +8,7 @@ class EpicsReport < ActiveRecord::Base
       INNER JOIN epics_stock_details s ON s.epics_stock_id = epics_stock_expiry_dates.epics_stock_details_id
       INNER JOIN epics_products p ON p.epics_products_id = s.epics_products_id  
       AND p.expire = 1").where("DATEDIFF(expiry_date,CURRENT_DATE())            
-      BETWEEN 1 AND 183").count(:expiry_date)                                   
+      BETWEEN 1 AND 183 AND current_quantity > 0").count(:expiry_date)                                   
                                                                                 
     alerts['Items below minimum stock'] += EpicsStockDetails.joins("           
       INNER JOIN epics_products p ON p.epics_products_id = epics_stock_details.epics_products_id

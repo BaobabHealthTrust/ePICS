b8c84a1586fca2c8044c344253c954aff8122ea9
Ernest Matola <mangochiman@gmail.com>
2013-08-23 11:42:41 +0200

    Working on global property migration

--- db/migrate/20130823083115_create_epics_global_property.rb
+++ db/migrate/20130823083115_create_epics_global_property.rb
@@ -1,11 +1,12 @@
 class CreateEpicsGlobalProperty < ActiveRecord::Migration
   def self.up
-    create_table :epics_global_property, :primary_key => :property do |t|
+    create_table :epics_global_property, :id => false do |t|
+      t.string :property
       t.string :property_value
       t.string :description
     end
+    execute "ALTER TABLE epics_global_property ADD PRIMARY key (property)"
   end
-
   def self.down
     drop_table :epics_global_property
   end

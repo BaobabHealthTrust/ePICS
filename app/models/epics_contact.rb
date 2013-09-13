be6863f99f8e9d97939b31e8655ef12f7657f5b4
Blessings <blessings.mhango@gmail.com>
2013-09-13 09:19:59 +0200

    sending email

--- app/models/epics_contact.rb
+++ app/models/epics_contact.rb
@@ -2,4 +2,14 @@ class EpicsContact < ActiveRecord::Base
   set_table_name :epics_contacts
   set_primary_key :epics_contact_id
   default_scope where("#{table_name}.voided = 0")
+
+  def self.send_email
+    EpicsContact.where(:voided => false).collect do |contact|
+      subject = "Epics Report"
+      body = "Dear #{contact.title} #{contact.first_name}  #{contact.last_name} <br /> Please find
+              attached a report for today"
+			Notifications.notify(contact,subject,body).deliver
+    end
+  end
+  
 end

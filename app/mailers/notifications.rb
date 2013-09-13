275fd22aee51df2d684f77d2abc871fda4a614de
Blessings <blessings.mhango@gmail.com>
2013-09-13 11:24:37 +0200

    unsent email report

--- app/mailers/notifications.rb
+++ app/mailers/notifications.rb
@@ -1,16 +1,22 @@
 class Notifications < ActionMailer::Base
 	include SendGrid
-  default :from => "dde@baobabhealth.org"
+  default :from => "epics@baobabhealth.org"
   sendgrid_category :use_subject_lines
   sendgrid_enable   :ganalytics, :opentrack
   sendgrid_unique_args :key1 => Time.now, :key2 => Time.now
 
-  def notify(contact,subject,body)
+  def notify(contact,subject,body,file_name)
 	 sendgrid_category "Report"
 	 sendgrid_unique_args :key2 => Time.now, :key3 => Time.now
-   @subject = subject
-   @email_body = body
-   mail(:to => contact.email_address, :subject => @subject)
+     attachments["#{file_name}"] = File.read("/tmp/#{file_name}") rescue nil
+     @subject = subject
+     @email_body = body
+     mail(:to => contact.email_address, :subject => @subject)
+  end
+
+  def email_error(subject)
+    @subject = subject
+    mail(:to => "epics@baobabhealth.org", :subject => @subject)
   end
 
 end
--- app/models/epics_contact.rb
+++ app/models/epics_contact.rb
@@ -4,12 +4,18 @@ class EpicsContact < ActiveRecord::Base
   default_scope where("#{table_name}.voided = 0")
 
   def self.send_email
-    EpicsContact.where(:voided => false).collect do |contact|
-      subject = "Epics Report"
-      body = "Dear #{contact.title} #{contact.first_name}  #{contact.last_name} <br /> Please find
-              attached a report for today"
-			Notifications.notify(contact,subject,body).deliver
-    end
+    file_name = 'medical_records1.pdf'
+    if File.exist?("/tmp/#{file_name}")
+      EpicsContact.where(:voided => false).each do |contact|
+        subject = "Epics Report"
+        body = "Dear #{contact.title} #{contact.first_name}  #{contact.last_name} <br /><br /> Please find
+                attached a report for today"
+        Notifications.notify(contact,subject,body,file_name).deliver
+      end
+    else
+      subject = "Epics Email Error"
+      Notifications.email_error(subject).deliver
+    end 
   end
   
 end

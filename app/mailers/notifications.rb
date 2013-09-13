07624e2917a76df8673bc59223c3c43197dfd705
Blessings <blessings.mhango@gmail.com>
2013-09-13 09:19:32 +0200

    sending email

--- app/mailers/notifications.rb
+++ app/mailers/notifications.rb
@@ -1,6 +1,6 @@
 class Notifications < ActionMailer::Base
 	include SendGrid
-  default :from => "epics@baobabhealth.org"
+  default :from => "dde@baobabhealth.org"
   sendgrid_category :use_subject_lines
   sendgrid_enable   :ganalytics, :opentrack
   sendgrid_unique_args :key1 => Time.now, :key2 => Time.now
@@ -8,16 +8,9 @@ class Notifications < ActionMailer::Base
   def notify(contact,subject,body)
 	 sendgrid_category "Report"
 	 sendgrid_unique_args :key2 => Time.now, :key3 => Time.now
-   sendgrid_recipients contacts_to_notify
    @subject = subject
    @email_body = body
-   mail(:to => "kch.epics@gmail.com", :subject => @subject)
+   mail(:to => contact.email_address, :subject => @subject)
   end
 
-  def contacts_to_notify
-    contacts = EpicsContact.where(:voided => false).collect do |contact|
-      contact.email_address
-    end
-    return contacts
-  end
 end

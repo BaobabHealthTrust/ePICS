55c94a5467f4e9f861057ea8e301b72f8946b94f
Blessings <blessings.mhango@gmail.com>
2013-09-13 08:38:47 +0200

    working on reports

--- app/mailers/notifications.rb
+++ app/mailers/notifications.rb
@@ -1,24 +1,23 @@
 class Notifications < ActionMailer::Base
 	include SendGrid
-  default :from => User.find_by_name("admin").email
+  default :from => "epics@baobabhealth.org"
   sendgrid_category :use_subject_lines
   sendgrid_enable   :ganalytics, :opentrack
   sendgrid_unique_args :key1 => Time.now, :key2 => Time.now
 
-  def notify(user,site_name,subject,body)
-	 sendgrid_category "Notification"
+  def notify(contact,subject,body)
+	 sendgrid_category "Report"
 	 sendgrid_unique_args :key2 => Time.now, :key3 => Time.now
-   sendgrid_recipients users_to_notify
-   @site_name = site_name
+   sendgrid_recipients contacts_to_notify
    @subject = subject
    @email_body = body
-   mail(:to => User.find_by_name("admin").email, :subject => @subject)
+   mail(:to => "kch.epics@gmail.com", :subject => @subject)
   end
 
-  def users_to_notify
-    users = User.where(:disabled => 0, :notify => 1).collect do |user|
-      user.email
+  def contacts_to_notify
+    contacts = EpicsContact.where(:voided => false).collect do |contact|
+      contact.email_address
     end
-    return users
+    return contacts
   end
 end
